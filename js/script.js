4ab6e3a977d064dba78044f24679d18523537c42
KamQuoss <kamilaklosek@gmail.com>
2020-06-12 16:12:04 +0200

    fuel cost added, graphic corrections

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// Praca domowa:
-// Przygotować kalkulator porównania opłacalności samochodu elektryczny i spalinowy
-// Samochód spalinowy spala 5l /100km w trasie a w mieście 8l/100km
-// Samochód z napędem hybrydowym spala 7l /100km w trasie a w mieście 4l/100km
-// Przygotować kalkulator porównawczy w zależności od ilości km które przejeżdżamy w mieście i na trasie miesięcznie
-// Kalkulator oblicza ile pieniędzy wydamy na jeden samochód i na drugi i pokazuje który jest bardziej opłacalny
-// wygląd podobny do wyglądu jak na schemacie
-
-
-// Comb like Combustion
 let
     cityRange = document.querySelector('input#city-range'),
     roadRange = document.querySelector('input#road-range'),
@@ -18,57 +8,61 @@ let
     combustionCarCityComb = 8 / 100, // l/km
     combustionCarRoadComb = 5 / 100, // l/km
     hybridCarCityComb = 4 / 100, // l/km
-    hybridCarRoadComb = 7 / 100; // l/km
+    hybridCarRoadComb = 7 / 100, // l/km
     effectiveness = document.querySelector('.effectiveness'),
     fuelPrice = document.querySelector('#fuel-price'),
     fuelPriceValue = fuelPrice.value,
-    costDifference = document.querySelector('.cost-difference')
-
+    costDifference = document.querySelector('.cost-difference');
 
 setValue = (value, valueContainer) => {
-    valueContainer.innerHTML = value
-}
+    valueContainer.innerHTML = `<b>${value}</b>`
+};
 
 combustionCarCost = () => {
-    return ( (cityValue * combustionCarCityComb) + (roadValue * combustionCarRoadComb) ) * fuelPriceValue
-}
+    return ((cityValue * combustionCarCityComb) + (roadValue * combustionCarRoadComb)) * fuelPriceValue
+};
 
 hybridCarCost = () => {
-    return ( (cityValue * hybridCarCityComb) + (roadValue * hybridCarRoadComb) ) * fuelPriceValue
-}
+    return ((cityValue * hybridCarCityComb) + (roadValue * hybridCarRoadComb)) * fuelPriceValue
+};
 
 checkCost = () => {
-    if ( combustionCarCost() > hybridCarCost() ) {
+    if (combustionCarCost() == hybridCarCost()) {
+        effectiveness.innerHTML = 'no difference';
+        let difference = Number(combustionCarCost() - hybridCarCost()).toFixed(2);
+        costDifference.innerHTML = `${difference} €`
+    } 
+    else if (combustionCarCost() > hybridCarCost()) {
         effectiveness.innerHTML = 'hybrid car';
-        let difference = Number(combustionCarCost() - hybridCarCost()).toFixed(2); 
+        let difference = Number(combustionCarCost() - hybridCarCost()).toFixed(2);
         costDifference.innerHTML = `${difference} €`
     }
     else {
         effectiveness.innerHTML = 'combustion car';
-        let difference = Number(hybridCarCost() - combustionCarCost()).toFixed(2); 
+        let difference = Number(hybridCarCost() - combustionCarCost()).toFixed(2);
         costDifference.innerHTML = `${difference} €`
     }
-}
+};
 
 init = () => {
     setValue(cityValue, cityKm);
     setValue(roadValue, roadKm);
     checkCost();
-}
+};
 
-cityRange.addEventListener('change', (e) => {
+cityRange.addEventListener('input', (e) => {
     cityValue = e.target.value;
     setValue(cityValue, cityKm);
     checkCost()
-})
-roadRange.addEventListener('change', (e) => {
+});
+roadRange.addEventListener('input', (e) => {
     roadValue = e.target.value;
     setValue(roadValue, roadKm);
     checkCost()
-})
+});
 fuelPrice.addEventListener('change', (e) => {
     fuelPriceValue = e.target.value;
     checkCost()
-})
+});
 
 init()
